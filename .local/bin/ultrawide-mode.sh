ae5c3109fa8b556be173bc827f4f9bedee6d6efa
cperez-one-two <christopher12perez6@gmail.com>
2022-03-14 21:47:08 -0400

    fix zsh prompt bug

--- .local/bin/ultrawide-mode.sh
+++ .local/bin/ultrawide-mode.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-xrandr --newmode "2560x1080_42" 155.50 2560 2688 2952 3344 1080 1083 1093 1109 -hsync +vsync
+xrandr --newmode "2560x1080-42" 155.50 2560 2688 2952 3344 1080 1083 1093 1109 -hsync +vsync
 
-xrandr --addmode "HDMI2" 2560x1080_42
+xrandr --addmode "HDMI2" 2560x1080-42
 
-xrandr --output LVDS1 --auto --pos 1920x1080 --output HDMI2 --mode 2560x1080_42 --pos 0x0
+xrandr --output LVDS1 --auto --pos 1920x1080 --output HDMI2 --mode 2560x1080-42 --pos 0x0
