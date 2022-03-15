b22db123b7353f8bab1bcf2faac718b603997162
cperez-one-two <christopher12perez6@gmail.com>
2022-03-14 20:48:33 -0400

    fix polybar disk module bug

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,26 +6,28 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 0.5; done
 
 outputs=$(xrandr --query | grep " connected" | cut -d" " -f1)
 
-if [ "$(echo $outputs | head -n1)" == "LVDS1" ]; then
-	tray_output=LVDS1
+if [ "$(hostname)" == "meep" ]; then
+    tray_output=LVDS1
+    mount='/home'
 else
-	tray_output=eDP-1
+    tray_output=eDP-1
+    mount='/'
 fi
 
 
 for m in $outputs; do
-	case $m in
-		*HDMI*)
-			tray_output=$m
-			;;
-	esac
+    case $m in
+	*HDMI*)
+	    tray_output=$m
+	    ;;
+    esac
 done
 
 for m in $outputs ; do
-	if [[ $m == $tray_output ]]; then
-		TRAY_POSITION=right MONITOR=$m polybar --reload cwmbar 2>~/.cache/polybar/$m.log &
-	else
-		TRAY_POSITION=none MONITOR=$m polybar --reload cwmbar 2>~/.cache/polybar/$m.log &
-	fi
+    if [[ $m == $tray_output ]]; then
+	TRAY_POSITION=right MONITOR=$m MOUNTPOINT=$mount polybar --reload cwmbar 2>~/.cache/polybar/$m.log &
+    else
+	TRAY_POSITION=none MONITOR=$m MOUNTPOINT=$mount polybar --reload cwmbar 2>~/.cache/polybar/$m.log &
+    fi
 done
 #polybar --reload example 2>~/.cache/polybar/$m.log &
