0258009b059fb142bf82cd5536e2c18d66513fcf
cperez-one-two <christopher12perez6@gmail.com>
2022-03-14 20:33:39 -0400

    fix polybar launch

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -5,13 +5,20 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 0.5; done
 
 outputs=$(xrandr --query | grep " connected" | cut -d" " -f1)
-tray_output=eDP-1
+
+if [ "$(echo $outputs | head -n1)" == "LVDS1" ]; then
+	tray_output=LVDS1
+else
+	tray_output=eDP-1
+fi
 
 
 for m in $outputs; do
-	if [[ $m == "HDMI-2" ]]; then
-		tray_output=$m
-	fi
+	case $m in
+		*HDMI*)
+			tray_output=$m
+			;;
+	esac
 done
 
 for m in $outputs ; do
