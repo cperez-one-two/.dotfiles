f8990cd908b06c7832c43db2d778043e93be0ebd
Chris P <chris.perez@wizehive.com>
2022-08-04 11:12:37 -0400

    add emacs cop profile, update gitignore

--- .emacs-profiles.el
+++ .emacs-profiles.el
@@ -1,3 +1,4 @@
 (("default"   . ((user-emacs-directory . "~/.emacs.efs")))
  ("doom" . ((user-emacs-directory . "~/.emacs.doom")))
-             (env . (("DOOMDIR" . "~/.config/doom"))))
+ (env . (("DOOMDIR" . "~/.config/doom")))
+ ("cop" . ((user-emacs-directory . "~/.emacs.cop"))))
