7a94f68881f04e261cdef103b9ee60c821acc309
Chris P <chris.perez@wizehive.com>
2022-10-22 20:34:38 -0600

    add consult package to completion

--- .config/chrispmacs/modules/completion.el
+++ .config/chrispmacs/modules/completion.el
@@ -2,8 +2,10 @@
 
 ;;; Dependencies
 (cop-install-package-if-not-already 'vertico)
+(cop-install-package-if-not-already 'consult)
 (cop-install-package-if-not-already 'savehist)
 (cop-install-package-if-not-already 'marginalia)
+
 
 (require 'vertico)
 (vertico-mode)
@@ -12,7 +14,12 @@
 (require 'savehist)
 (savehist-mode)
 
+(require 'marginalia)
 (with-eval-after-load 'vertico
   (marginalia-mode))
+
+(require 'consult)
+(setq completion-styles '(substring basic))
+(global-set-key (kbd "C-s") 'consult-line)
 
 (provide 'completion)
